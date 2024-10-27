4e3ee166a3178004ccdd442a180ff5dfad4ac532
Rahul <ayushtri56@gmail.com>
2024-10-27 23:27:46 +0530

    Infinite comment

--- coding/src/pages/react/infinite-comments/Comment.jsx
+++ coding/src/pages/react/infinite-comments/Comment.jsx
@@ -1,13 +1,65 @@
-export default function Comment(){
-    return (
-        <div>
-            <div>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos autem, ratione porro minus excepturi fugit maiores eligendi fuga enim sed illum, facilis minima quibusdam omnis. Inventore, facilis! Delectus, impedit magni!
-            </div>
-            <div>
-                <span>View Reply</span>
-                <span>Add Reply</span>
-            </div>
+import { useState } from "react";
+import styles from "./styles.module.css";
+
+export default function Comment({ commentData }) {
+  return (
+    <>
+      {commentData.map((comment) => {
+        return <CommentItem comment={comment} />;
+      })}
+    </>
+  );
+}
+
+function CommentItem({ comment }) {
+  const [showReply, toggleReply] = useState(false);
+  const [showAddReply, toggleAddReply] = useState(false);
+  const [commentData, setCommentData] = useState(comment);
+
+  const onBlurInput = (e) => {
+    const _commentData = { ...commentData };
+    const commentVal = e.target.value;
+    const commentObj = {
+      id: "fsfs" + Math.random(),
+      comment: commentVal,
+      subComments: [],
+    };
+
+    if (_commentData.subComments.length) {
+      _commentData.subComments.push(commentObj);
+    } else {
+      _commentData.subComments = [commentObj];
+    }
+
+    setCommentData(_commentData);
+    toggleAddReply(prev => !prev)
+    toggleReply(true);
+  };
+
+  return (
+    <>
+      <div className={styles.commentContainer}>
+        <div className={styles.commentDetails}>
+          <div>{commentData.comment}</div>
+          <div className={styles.controls}>
+            {commentData.subComments?.length > 0 && (
+              <span onClick={() => toggleReply((prev) => !prev)}>
+                {!showReply ? "View Reply" : "Hide Reply"}
+              </span>
+            )}
+            <span onClick={() => toggleAddReply(!showAddReply)}>Add Reply</span>
+          </div>
         </div>
-    )
+        {showAddReply && (
+          <input
+            type="text"
+            placeholder="Enter your comment"
+            className={styles.commentBox}
+            onBlur={onBlurInput}
+          />
+        )}
+        {showReply && <Comment commentData={commentData.subComments} />}
+      </div>
+    </>
+  );
 }
--- coding/src/pages/react/infinite-comments/index.jsx
+++ coding/src/pages/react/infinite-comments/index.jsx
@@ -1,52 +1,8 @@
-import React, { useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
-import CommentForm from '/src/components/InfiniteCommentComponents/CommentForm/index'
-import CommentList from '/src/components/InfiniteCommentComponents/CommentList'
+import Comment from "./Comment";
+import commentsData from "./comment_data";
 
-const App = () => {
-  const [comments, setComments] = useState([]);
-
-  const handleAddComment = (text) => {
-    setComments([...comments, { id: uuidv4(), text, replies: [] }]);
-  };
-
-  const handleEditComment = (id, newText) => {
-    setComments(comments.map(comment => 
-      comment.id === id ? { ...comment, text: newText } : comment
-    ));
-  };
-
-  const handleDeleteComment = (id) => {
-    setComments(comments.filter(comment => comment.id !== id));
-  };
-
-  const handleReplyComment = (id, replyText) => {
-    const addReply = (comments) => {
-      return comments.map(comment => {
-        if (comment.id === id) {
-          return { ...comment, replies: [...comment.replies, { id: uuidv4(), text: replyText, replies: [] }] };
-        } else if (comment.replies) {
-          return { ...comment, replies: addReply(comment.replies) };
-        } else {
-          return comment;
-        }
-      });
-    };
-    setComments(addReply(comments));
-  };
-
+export default function(){
   return (
-    <div className="App">
-      <h1>Infinite Comments</h1>
-      <CommentForm onAdd={handleAddComment} />
-      <CommentList 
-        comments={comments} 
-        onEdit={handleEditComment} 
-        onDelete={handleDeleteComment} 
-        onReply={handleReplyComment} 
-      />
-    </div>
-  );
-};
-
-export default App;
+    <Comment commentData = {commentsData}/>
+  )
+}
